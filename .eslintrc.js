64aed443bae0b6197700c7bc878d31cc335aee4a
JaeSeoKim <devjaeseo@gmail.com>
2022-05-09 14:18:29 +0900

    :green_heart: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['plugin:import/errors', 'plugin:import/warnings', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
   rules: {},
 }
